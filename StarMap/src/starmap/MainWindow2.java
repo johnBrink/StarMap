bbbd00eec8a820df2dc4cec176dc45a649a03616
MattRam <mpr6280@gmail.com>
2014-11-04 10:06:48 -0700

    Updated MainWindow2

--- StarMap/src/starmap/MainWindow2.java
+++ StarMap/src/starmap/MainWindow2.java
@@ -5,6 +5,7 @@
  */
 package starmap;
 
+
 /**
  *
  * @author 7101020
@@ -16,6 +17,7 @@ public class MainWindow2 extends javax.swing.JFrame {
      */
     public MainWindow2() {
         initComponents();
+        this.setDefaultCloseOperation(MainWindow2.EXIT_ON_CLOSE);
     }
 
     /**
@@ -27,21 +29,62 @@ public class MainWindow2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Display = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        QuitButton = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        ViewMenu = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Display.setBackground(new java.awt.Color(0, 0, 0));
+        Display.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        javax.swing.GroupLayout DisplayLayout = new javax.swing.GroupLayout(Display);
+        Display.setLayout(DisplayLayout);
+        DisplayLayout.setHorizontalGroup(
+            DisplayLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+        DisplayLayout.setVerticalGroup(
+            DisplayLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 379, Short.MAX_VALUE)
+        );
+
+        QuitButton.setText("File");
+
+        jMenuItem1.setText("Quit");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        QuitButton.add(jMenuItem1);
+
+        jMenuBar1.add(QuitButton);
+
+        ViewMenu.setText("View");
+        jMenuBar1.add(ViewMenu);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(Display, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(Display, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +122,10 @@ public class MainWindow2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Display;
+    private javax.swing.JMenu QuitButton;
+    private javax.swing.JMenu ViewMenu;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
     // End of variables declaration//GEN-END:variables
 }
