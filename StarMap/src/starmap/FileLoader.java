ba2b7d1331a3ca9935d39f7ea3baf080fb48d052
MattRam <mpr6280@gmail.com>
2014-11-04 11:34:16 -0700

    added resources folder

--- StarMap/src/starmap/FileLoader.java
+++ StarMap/src/starmap/FileLoader.java
@@ -20,8 +20,6 @@ public class FileLoader {
     {
         ArrayList<Star> stars = new ArrayList<>();
         ArrayList<Constellation> constellations = new ArrayList<>();
-        System.out.print(starsFile);
-        System.out.print("\n");
         // Find all the stars
         try
         {
